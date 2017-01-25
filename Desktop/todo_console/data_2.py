007ad982a87b48b0a24f4badcd6fdf2cadae4bc9
Lavender Anyango Ayodi <layodi749@gmail.com>
2017-01-25 11:09:40 -0800

    complete working functions and methods

--- Desktop/todo_console/data_2.py
+++ Desktop/todo_console/data_2.py
@@ -1,6 +1,6 @@
 import sys
 import sqlite3
-from collections import defaultdict
+import itertools
 
 
 con = sqlite3.connect('database_2.db')
@@ -15,39 +15,65 @@ create_table()
 #def hjt():
 	#b_list = c.fetchall()
 
+def data_entry():
+    #user = input('Enter your first name: ')
+    todo = input('Enter the activity you want to do:')
+    item = input('Enter the item you want to do:')
+    c.execute ('INSERT INTO toDo_4 (todo, item) VALUES (?, ?)', (todo, item))
+    con.commit()
+    print ('Data entered successfully')
+    que = input ('Do you want add another item? y/n ')
+    print ('Data entered successfully')
+    while que == 'y':
+	    todo = input('Enter the activity you want to do:')
+	    item = input('Enter the item you want to do:')
+	    c.execute ('INSERT INTO toDo_4 (todo, item) VALUES (?, ?)', (todo, item))
+	    con.commit()
+	    print ('Data entered successfully')
+	    que = input ('Do you want add another item? y/n ')
+	    
+	    if que == 'n':
+		    break
 
-#user = input('Enter your first name: ')
-todo = input('Enter the activity you want to do:')
-item = input('Enter the item you want to do:')
-c.execute ('INSERT INTO toDo_4 (todo, item) VALUES (?, ?)', (todo, item))
-con.commit()
-print ('Data entered successfully')
-que = input ('Do you want add another item? y/n')
-print ('Data entered successfully')
-while que == 'y':
-	todo = input('Enter the activity you want to do:')
-	item = input('Enter the item you want to do:')
-	c.execute ('INSERT INTO toDo_4 (todo, item) VALUES (?, ?)', (todo, item))
-	con.commit()
-	print ('Data entered successfully')
-	que = input ('Do you want add another item? y/n')
-	
-	if que == 'n':
-		break
+data_entry()
 
 def view_todo ():
-	c.execute('SELECT * FROM toDo_4')
+	con.row_factory = lambda cursor, row: row [0]
+	rows = c.execute('SELECT todo FROM toDo_4').fetchall()
+	#return (rows)
+	
 
+	for i in rows:
+		a = []
+		if type(i) == (tuple):
+			for j in i:
+				a.append(j)
+
+				print (a)
+
+
+def solve():
+	c = con.cursor()
+	con.row_factory = lambda cursor, row: row [0]
+	c.execute('SELECT todo,count (todo) from toDo_4 group by todo having count (todo) >1')
+	k = c.fetchall()
+
+	print (k)
+
+solve()
+
+
+def view_items():
+	c.execute('SELECT * FROM toDo_4')
 	rows = c.fetchall()
 
 	for row in rows:
-		print (row)
+		print (row [1])
 
-		d = defaultdict(dict)
-		for a, b in row:
-			d[a] = b
-			print (d)
-view_todo()
+view_items()
+
+
+
 
 
 
--- Desktop/todo_console/todo.py
+++ Desktop/todo_console/todo.py
@@ -8,3 +8,13 @@ def create_table():
 	c.execute('CREATE TABLE IF NOT EXISTS toDoList(userid TEXT, todo TEXT, items TEXT )')
 
 	create_table()
+	def view_todo ():
+	con.row_factory = lambda cursor, row: row [0]
+	rows = c.execute('SELECT todo FROM toDo_4').fetchall()
+	print (rows)
+
+		#d = defaultdict(dict)
+		#for a, b in row:
+			#d[a] = b
+			#print (d)
+view_todo()
